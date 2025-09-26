cefbaa341fc14f731ca984a891253d27aeb92151
arunjit <arunjit@ajsd.in>
2025-09-26 23:30:56 +0100

    Add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,24 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- Make line numbers default
+vim.o.number = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = 'a'
+
+require("config.lazy")
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
